a3ec2692f855cfa12396b8f8c9189ebff593efd9
dilanarango <dilan.arango@uao.edu.co>
2021-11-17 21:45:29 -0500

    component
     login-css1

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< Updated upstream
-=======
-import Navbar from './Components/Navbar/Navbar';
+
 import Login_s from './Components/login/login';
->>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
       <Login_s/>
       
->>>>>>> Stashed changes
     </div>
   );
 }
