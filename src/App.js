f1e4a6404de24f3e4e69c934c5c982e47d75b92e
Kevin Romero Bedoya <kr38996@gmail.com>
2021-11-19 07:28:32 -0500

    Login funcional

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import { Route, Routes } from 'react-router';
 import './App.css';
 import Calendar from './pages/Calendar';
 import Home from './pages/Home';
-import Login from './pages/Login';
+import Login from './pages/Login'
 
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/Home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/calendar" element={<Calendar />} />
       </Routes>
