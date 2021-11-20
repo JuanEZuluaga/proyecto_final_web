03f1cc8d901821f5952199733e0d636dd4eda514
danielcotes2504 <danielenrique2000@hotmail.com>
2021-11-19 20:45:34 -0500

    Ultimo commit sagrado
    Se hizo

--- src/pages/Reportes.js
+++ src/pages/Reportes.js
@@ -16,13 +16,13 @@ const Reportes = () => {
             <Navlayout>
                 <div className='container mx-auto'>
                     <div className='text-3xl font-bold text-left py-12'>
-                        <h1>REPORTES</h1>
+                        <h1 className='text-gray-500'>Reportes</h1>
                     </div>
                     <div className='space-y-14'>
                         {data.length > 0 && <ReportsTable data={data} />}
                         <div className="filter drop-shadow-xl overflow-hidden border-b bg-white border-gray-200 sm:rounded-lg px-10 divide-y divide-gray-400-500">
                             <div className='text-2xl font-bold text-left pt-8 pb-2'>
-                                <h2>FECHA DE COHORTE 2021-03</h2>
+                                <h2 className='text-gray-500'>Fecha de corte 2021-03</h2>
                             </div>
                             <div className='pt-4 pb-8 flex flex-col items-start space-y-4'>
                                 <span><strong>Fecha de entrega:</strong> Sábado, 30 de octubre del 2021, 11:59PM</span>
