589fc1e82f47031f7875dd977278bd036e4176e1
danielcotes2504 <danielenrique2000@hotmail.com>
2021-11-19 19:02:45 -0500

    Report State
    Ahora registra las horas totales

--- src/Components/Calendar/Sidebar/Sidebar.js
+++ src/Components/Calendar/Sidebar/Sidebar.js
@@ -2,12 +2,14 @@ import React from 'react'
 import CreateEventButton from "./CreateEventButton/CreateEventButton"
 import SmallCalendar from './SmallCalendar/SmallCalendar';
 import Labels from './Labels/Labels'
+import ReportState from './ReportState/ReportState';
 function Sidebar() {
     return (
         <aside className="border p-5 w-64">
             <CreateEventButton />
             <SmallCalendar />
             <Labels />
+            <ReportState/>
 
         </aside>
     )
